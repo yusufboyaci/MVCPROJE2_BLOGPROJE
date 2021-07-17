cb6b56b9281e389a412d9a0c3ecc6e7ce00b9ac7
yusufboyaci <boyacyusuf@gmail.com>
2021-07-17 11:54:40 +0300

    İlk Commit

---
+++ MVCPROJE2_BLOGPROJE.SERVICES/Repositories/Concrete/MakaleRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCPROJE2_BLOGPROJE.SERVICES.Repositories.Concrete
+{
+    class MakaleRepository
+    {
+    }
+}
