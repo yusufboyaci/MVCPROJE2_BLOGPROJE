d99e3b586835aca273ad08308c6a6ca87920dcff
yusufboyaci <boyacyusuf@gmail.com>
2021-07-17 15:17:41 +0300

    Uye ve Makale Repository ler eklendi

--- MVCPROJE2_BLOGPROJE.SERVICES/Repositories/Concrete/MakaleRepository.cs
+++ MVCPROJE2_BLOGPROJE.SERVICES/Repositories/Concrete/MakaleRepository.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using MVCPROJE2_BLOGPROJE.CORE.Entities;
+using MVCPROJE2_BLOGPROJE.DATAACCESS.Context;
+using MVCPROJE2_BLOGPROJE.SERVICES.Repositories.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,42 @@ using System.Threading.Tasks;
 
 namespace MVCPROJE2_BLOGPROJE.SERVICES.Repositories.Concrete
 {
-    class MakaleRepository
+    public class MakaleRepository : IMakaleRepository
     {
+        private readonly ApplicationDbContext _context;
+        public MakaleRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IQueryable<Makale> Makaleler => _context.Makaleler;
+
+        public Makale GetById(int id) => _context.Makaleler.Find(id);
+
+
+        public List<Konu> KonuListesi() => _context.Konular.ToList();
+        
+        public bool MakaleEkle(Makale makale)
+        {
+            _context.Makaleler.Add(makale);
+            return _context.SaveChanges() > 0;
+        }
+
+        public bool MakaleGuncelle(Makale makale)
+        {
+            _context.Makaleler.Update(makale);
+            return _context.SaveChanges() > 0;
+        }
+
+        public List<Makale> MakaleKonuDahilEt() => _context.Makaleler.Include(x => x.KonularMakaleler).ToList();
+      
+
+        public bool MakaleSil(int id)
+        {
+            _context.Makaleler.Remove(GetById(id));
+            return _context.SaveChanges() > 0;
+        }
+
+        public List<Makale> MakaleUyeDahilEt() => _context.Makaleler.Include(x => x.Uye).ToList();
+       
     }
 }
--- MVCPROJE2_BLOGPROJE.SERVICES/Repositories/Concrete/UyeRepository.cs
+++ MVCPROJE2_BLOGPROJE.SERVICES/Repositories/Concrete/UyeRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MVCPROJE2_BLOGPROJE.CORE.Entities;
+using MVCPROJE2_BLOGPROJE.DATAACCESS.Context;
+using MVCPROJE2_BLOGPROJE.SERVICES.Repositories.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,39 @@ using System.Threading.Tasks;
 
 namespace MVCPROJE2_BLOGPROJE.SERVICES.Repositories.Concrete
 {
-    class UyeRepository
+    public class UyeRepository : IUyeRepository
     {
+        private readonly ApplicationDbContext _context;
+        public UyeRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IQueryable<Uye> Uyeler => _context.Uyeler;
+
+        public Uye GetById(int id)
+        {
+            return _context.Uyeler.Find(id);
+        }
+
+        public bool UyeEkle(Uye uye)
+        {
+            _context.Uyeler.Add(uye);
+           // _context.Makaleler.Where(x => x.UyeID == x.ID);
+            return _context.SaveChanges() > 0;
+        }
+
+        public bool UyeGuncelle(Uye uye)
+        {
+            _context.Uyeler.Update(uye);
+            return _context.SaveChanges() > 0;
+        }
+
+        public bool UyeKaydet() => _context.SaveChanges() > 0;
+
+        public bool UyeSil(int id)
+        {
+            _context.Uyeler.Remove(GetById(id));
+            return _context.SaveChanges() > 0;
+        }
     }
 }
